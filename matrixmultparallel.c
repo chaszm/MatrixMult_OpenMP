f61f7c8c9f5988edebcd7446c6401281910124ef
Chase <123278965+chaszm@users.noreply.github.com>
2023-11-02 23:56:06 -0400

    Add files via upload

---
+++ matrixmultparallel.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <omp.h>
+#include <time.h>
+#include <stdlib.h>
+#define N 4096
+#define FactorIntToDouble 1.1
+
+double firstMatrix[N][N] = {0.0};
+double secondMatrix[N][N] = {0.0};
+double matrixMultiResult[N][N] = {0.0};
+
+
+
+void matrixMulti() {
+ 
+ 	#pragma omp parallel for collapse(2)
+ 	
+    for ( int row=0 ; row < N; row++) {
+        for (int col=0 ; col < N; col++) {
+            double resultValue = 0;
+            
+            for (int transNumber = 0; transNumber < N; transNumber++) {
+                resultValue += firstMatrix[row][transNumber] * secondMatrix[transNumber][col];
+              
+            }
+            
+            matrixMultiResult[row][col] = resultValue;
+        }
+    }
+    //end of parallel
+}
+
+
+
+void matrixInit() {
+	#pragma omp parallel for collapse(2)
+    for (int row = 0; row < N; row++) {
+        for (int col = 0; col < N; col++) {
+            firstMatrix[row][col] = (row + col) * FactorIntToDouble;
+            secondMatrix[row][col] = (row + col) * FactorIntToDouble;
+        }
+    }
+    }
+int main()
+{
+matrixInit();
+
+double t1 = omp_get_wtime();
+matrixMulti();
+double t2 = omp_get_wtime();
+printf("parallel time: %3f\n", ((double)t2 - t1) / CLOCKS_PER_SEC *
+1000000.0);
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
